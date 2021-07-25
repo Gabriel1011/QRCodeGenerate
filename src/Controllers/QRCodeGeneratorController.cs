65459cc0fd319050e557b564c09ec58ec14b8a36
Gabriel Rodrigues <38117500+Gabriel1011@users.noreply.github.com>
2021-07-25 20:15:35 -0300

    Criando API pra gerar QRCode

---
+++ src/Controllers/QRCodeGeneratorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCodeGenerator.Services;
+
+namespace QRCodeGenerator.Controllers
+{
+    public class QRCodeGeneratorController : Controller
+    {
+        [HttpGet("qrcode/{text}")]
+        public IActionResult GetQrCode(string text)
+        {
+            var image = QrCodeGenerator.GenerateByteArray(text);
+            return File(image, "image/jpeg");
+        }
+    }
+}
---
+++ src/Services/QrCodeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace QRCodeGenerator.Services
+{
+    public static class QrCodeGenerator
+    {
+        public static Bitmap GenerateImage(string text)
+        {
+            var qrGenerator = new QRCoder.QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(text, QRCoder.QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(10);
+            return qrCodeImage;
+        }
+
+        public static byte[] GenerateByteArray(string url)
+        {
+            var image = GenerateImage(url);
+            return ImageToByte(image);
+        }
+
+        private static byte[] ImageToByte(Image img)
+        {
+            using var stream = new MemoryStream();
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
